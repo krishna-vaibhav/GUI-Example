afbdf36a2b80df3da9f44de8da46bcb297443652
krishna.vaibhav <krishnavaibhav01@gmail.com>
2019-06-28 13:15:53 +0530

    File Saved

---
+++ basic-html/code.js
@@ -0,0 +1,8 @@
+document.querySelector('button').addEventListener("click",function(){
+  document.querySelector('h2').innerHTML = "Welcome in Basics of HTML,CSS and JavaScript";
+});
+
+
+document.querySelector('button1').addEventListener("click",function(){
+  document.querySelector('h2').innerHTML = "Click on Button & see the change here.";
+});
